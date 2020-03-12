7b41cd72097f177c6fe1966af78bfbb570cb5e7d
Marco de Biolley <marcdebiolley@gmail.com>
2020-03-12 11:31:02 +0100

    question rails  exo

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,16 @@
+class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(params.require(:review).permit(:content, :rating))
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+end
---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese',
+  'french', 'belgian'] }
+  validates :address, presence: true
+  validates :category, presence: true
+end
